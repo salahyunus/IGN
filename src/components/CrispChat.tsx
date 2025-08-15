da8818d8f4354e4f68dae37142e5e0665df24eec
salahyunus <salahyunusnew@gmail.com>
2025-08-16 00:12:47 +0300

    ads

--- src/components/CrispChat.tsx
+++ src/components/CrispChat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export default function CrispChat() {
   useEffect(() => {
-    Crisp.configure("2a3e46f8-8dbc-4e62-a619-b8a808fde6e9");
+    Crisp.configure("cd3063a4-b86c-4dc2-bf93-04343da3e8f1");
   }, []);
 
   return null;
