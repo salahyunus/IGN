11350dc6d04f58977aa17582b2550b0d1af64f31
salahyunus <salahyunusnew@gmail.com>
2025-08-15 22:50:37 +0300

    n

--- src/components/CrispChat.tsx
+++ src/components/CrispChat.tsx
@@ -1,12 +1,12 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { Crisp } from "crisp-sdk-web";
+// import { useEffect } from "react";
+// import { Crisp } from "crisp-sdk-web";
 
-export default function CrispChat() {
-  useEffect(() => {
-    Crisp.configure("2a3e46f8-8dbc-4e62-a619-b8a808fde6e9");
-  }, []);
+// export default function CrispChat() {
+//   useEffect(() => {
+//     Crisp.configure("2a3e46f8-8dbc-4e62-a619-b8a808fde6e9");
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
